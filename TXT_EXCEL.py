83be1e972b2198e5e6691d59b1d88a018724b0f2
Nino <40154164+hwq37403@users.noreply.github.com>
2021-03-19 10:00:37 +0800

    Add files via upload
    Txt->EXCEL

---
+++ TXT_EXCEL.py
@@ -0,0 +1,22 @@
+import openpyxl
+
+
+def writetoxlsx():
+
+    data = open('E:\\WORK\\优惠小区\\6ka0000151.txt', 'r')
+    outwb = openpyxl.Workbook()  # 打开一个将写的文件
+    outws = outwb.create_sheet(index=0)  # 在将写的文件创建sheet
+
+    # print(data)
+    i = 1  # 注意：'cell'函数中行列起始值为1
+    for line in data:
+        print(line[0:5])
+        print(line[6:12])
+        outws.cell(column=2, row=i, value="%s" % line[0:5])
+        outws.cell(column=3, row=i, value="%s" % line[6:14])
+        i += 1
+    savexlsx = "E:\\WORK\\优惠小区\\6ka0000151.xlsx"
+    outwb.save(savexlsx)  # 保存结果
+    data.close()
+#
+writetoxlsx()
