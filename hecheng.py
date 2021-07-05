4fb10e51269dd32338b7c5be26314c6461170e81
Nino <40154164+hwq37403@users.noreply.github.com>
2021-07-05 23:59:02 +0800

    1111

--- hecheng.py
+++ hecheng.py
@@ -1,22 +1,29 @@
 import pandas as pd
 import os
-import sheet_name as sn
-
+#合成文件的上一级文件夹路径
+path = "C:\\Users\\去\\Documents"
+# 合成excel的列名
+list = ['电厂（交易对象）编码', '名称', '动作标记', '发行日期', '含税电费', '月份', '钱', '合并时间']
+#合成总表结果存放路径
+out_path=path+'\\合成总表.xlsx'
 
 def hecheng():
 	'''
-	合成多个子表，表名按1,2,3,4,5,6……的规则进行命名
-	:return:
+	合成多个子表，表路径为path下的xlsx文件,只需修改path，
+	:return: void
 	'''
-	sheets = sn.sheet_name()
+	sheets = os.listdir(path)  # 获取path目录下的所有excel
 	D = []
 	for x in sheets:
-		data = pd.read_excel('D:/财务数据/结果居民/' + str(x) + '.xlsx', sheet_name='Sheet1')
+		data = pd.read_excel(path + str(x) + '.xlsx', sheet_name='Sheet1')
 
 		print(data)
 		if data.empty is True:
 			continue
 		# data.rename(columns={'状态归类':'状态'},inplace=True)#状态归类和状态是同一个属性，所以我将状态归类替换成了状态
-		D.append(data.loc[:, ['电厂（交易对象）编码', '名称', '动作标记', '发行日期', '含税电费', '月份', '钱', '合并时间']])
+		D.append(data.loc[:, list])
 	num = pd.concat(D, axis=0)  # 合并list表D中的元素
-	num.to_excel('D:/财务数据/结果居民/居民台账总表.xlsx', index=False)
+	num.to_excel(out_path, index=False)
+
+if __name__ == '__main__':
+    hecheng()
